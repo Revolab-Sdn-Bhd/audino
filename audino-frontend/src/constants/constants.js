cc66f81e6a8d23638d47c4b0c8589672886227f4
Hendrick <hendrick.chong@revolab.ai>
2025-02-17 18:49:33 +0800

    feature: Edited Labels to fit Levantine Arabic contexts, added emotions label.

--- audino-frontend/src/constants/constants.js
+++ audino-frontend/src/constants/constants.js
@@ -11,7 +11,7 @@ export const DATASET_MAPING = {
   is_voxpopuli: "Gender, Age, Language/Locale, Start, End, Transcription",
   is_tedlium: "Gender, Language/Locale, Start, End, Transcription",
   is_commonvoice:
-    "Gender, Age, Accent, Language/Locale, Start, End, Transcription",
+    "Gender, Age, Accent, Emotion, Language/Locale, Start, End, Transcription",
 };
 
 export const OPTIONS_TASK_TYPE = [
